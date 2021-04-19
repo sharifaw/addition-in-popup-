e61707ab89e2a77383e3f554ef62907b24b06048
Opeto <opeto@opeto.home>
2021-04-19 11:06:54 +0300

    solve it in short

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,3 @@
-
-    let a = prompt("First number?", 4);
-    let b = prompt("Second number?", 9);
-    let c = 0;
-    alert (+a + +b || c);
+let a = prompt("First number?", 4);
+ let b = prompt("Second number?", 9);
+alert (+a + +b || 0);
